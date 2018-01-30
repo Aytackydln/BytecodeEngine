d266f8b803f170023bc0fc67cc347c08be8fcf8a
Aytackydln <robobok.chip@hotmail.com>
2018-01-31 00:46:51 +0300

    0.3.3

---
+++ src/test/java/Game/Maps/TutorialMap.java
@@ -0,0 +1,18 @@
+package Game.Maps;
+
+import Engine.Map;
+import Game.Main;
+import Game.Units.Monsters.Skeleton;
+import Game.Units.Player;
+
+public class TutorialMap extends Map{
+	public TutorialMap(){
+		super(500,500);
+		Main.engine.camera.viewHeight=500;
+		Main.engine.camera.viewWidth=500;
+		Main.engine.camera.updateScales();
+		name="Tutorial Map";
+		new Skeleton(this, 40,60);
+		new Player(this,25,25);
+	}
+}
